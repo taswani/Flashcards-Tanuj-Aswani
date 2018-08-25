d854a69fbf5a63f7ff4d0bbd07c2353909d9fad6
Tanuj Aswani <tanuj.aswani@gmail.com>
2018-08-24 20:51:44 -0700

    Minor Changes

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.locals.error = err
+  res.locals.error = err;
   res.status(err.status);
   res.render('error');
 });
